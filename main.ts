1b971812dfc54f9f96810f21ebc73e2d5b046247
sportyfred <ivanboring@gmail.com>
2025-12-01 23:13:25 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 import { chromium } from 'playwright-extra';
 import { BrowserContext } from 'playwright'; // Typen kommer från Playwrightimport * as fs from 'fs';
 
-const USERNAME = process.env.INSTAGRAM_USER;
-const PASSWORD = process.env.INSTAGRAM_PASSWORD;
+const USERNAME = ${{shared.INSTAGRAM_USER}};
+const PASSWORD = ${{shared.INSTAGRAM_PASSWORD}};
 const SESSION_FILE = 'instagram_session.json';
 
 
